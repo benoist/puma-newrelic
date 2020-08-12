8526ae96521c691c71f3f0d6033c334a0b76930d
Michal Kulesza <michal.kulesza@simplybusiness.co.uk>
2020-08-12 17:46:26 +0200

    Symbolize keys

--- lib/puma/new_relic/sampler.rb
+++ lib/puma/new_relic/sampler.rb
@@ -1,7 +1,7 @@
 module Puma
   module NewRelic
     class Sampler
-      KEYS = %w(backlog running pool_capacity max_threads)
+      KEYS = %i(backlog running pool_capacity max_threads)
 
       def initialize(launcher, sample_rate)
         @launcher       = launcher
@@ -41,10 +41,10 @@ module Puma
         metrics = Hash.new { |h, k| h[k] = 0 }
         sum     = ->(key, value) { metrics[key] += value if KEYS.include?(key) }
 
-        if stats["workers"]
-          metrics["workers"] = stats["workers"]
-          stats["worker_status"].each do |worker|
-            worker["last_status"].each(&sum)
+        if stats[:workers]
+          metrics[:workers] = stats[:workers]
+          stats[:worker_status].each do |worker|
+            worker[:last_status].each(&sum)
           end
         else
           stats.each(&sum)
